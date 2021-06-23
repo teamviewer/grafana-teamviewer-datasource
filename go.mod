3f1297cf837d837687eaae89a03828c11ee47893
Ryan McKinley <ryantxu@gmail.com>
2021-06-23 11:49:55 -0700

    minor instance manager cleanup

--- go.mod
+++ go.mod
@@ -2,11 +2,4 @@ module github.com/grafana/simple-datasource-backend
 
 go 1.16
 
-require (
-	github.com/grafana/grafana-plugin-sdk-go v0.101.0
-	github.com/jhump/protoreflect v1.6.1 // indirect
-	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
-	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
-	golang.org/x/text v0.3.5 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
-)
+require github.com/grafana/grafana-plugin-sdk-go v0.105.0
--- pkg/main.go
+++ pkg/main.go
@@ -11,9 +11,7 @@ func main() {
 	// Start listening to requests send from Grafana. This call is blocking so
 	// it won't finish until Grafana shutsdown the process or the plugin choose
 	// to exit close down by itself
-	err := datasource.Serve(newDatasource())
-	// Log any error if we could start the plugin.
-	if err != nil {
+	if err := datasource.Manage("teamviewer-datasource", NewWebMonitoringDatasource, datasource.ManageOpts{}); err != nil {
 		log.DefaultLogger.Error(err.Error())
 		os.Exit(1)
 	}
